b0809dbc49b6af112b87b9c5b2ed30dc8aeb483e
AlwaysDreamingBig <darkthereal1@gmail.com>
2024-12-07 22:13:17 +0100

    Cerify token function

---
+++ backend/src/common/utils/verify-token.ts
@@ -0,0 +1,74 @@
+import { config } from "../../config/app.config";
+import { HTTPSTATUS } from "../../config/http.config";
+import { throwHttpError } from "../../middleware/errorHandler";
+import { AppErrorMessage } from "../enums/app-error.enum";
+import { verifyJwtToken } from "./jwt";
+import jwt from "jsonwebtoken";
+
+
+// Function to verify refresh token
+/**
+ * 
+ * @param Token is the token to verify
+ * @param type is the type of token refresh | access
+ * @returns is the payload
+ */
+export async function verifyToken(Token: string, type: string) {
+    try {
+        // Decode the token without verifying
+    const decoded = jwt.decode(Token) as { role?: string };
+  
+      if (!decoded || !decoded.role) {
+        throwHttpError(
+          AppErrorMessage.AUTH_INVALID_TOKEN,
+          HTTPSTATUS.UNPROCESSABLE_ENTITY
+        );    
+      }
+  
+      const { role } = decoded;
+  
+      // Map user role to the appropriate secret
+      let secret: string;
+      switch (role) {
+        case "admin":
+            if(type === "refresh") {
+                secret = config.JWT.ADMIN_REFRESH_SECRET;
+            } else{
+                secret = config.JWT.ADMIN_SECRET;
+            }
+          break;
+        case "manager":
+          secret = config.JWT.MANAGER_REFRESH_SECRET;
+          break;
+        case "developer":
+          secret = config.JWT.DEV_REFRESH_SECRET;
+          break;
+        case "client":
+          secret = config.JWT.REFRESH_SECRET;
+          break;
+        default:
+          secret = config.JWT.REFRESH_SECRET; // Default for generic users
+      }
+  
+      // Fully verify the token with the mapped secret
+      const payload = verifyJwtToken(Token, {
+        key: secret,
+        audience: role, // Ensure audience matches the role
+      });
+  
+      if (!payload || !payload.sessionId) {
+        throwHttpError(
+          AppErrorMessage.AUTH_INVALID_TOKEN,
+          HTTPSTATUS.NO_CONTENT
+        );
+      }
+  
+      return payload; // Successfully verified payload
+    } catch (error) {
+      throwHttpError(
+        AppErrorMessage.AUTH_INVALID_TOKEN,
+        HTTPSTATUS.BAD_REQUEST
+      );
+    }
+  }
+  
