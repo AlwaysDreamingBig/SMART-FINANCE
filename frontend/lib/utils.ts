a101a443de543e4cc9e3024a2e675ec4b51a1b48
AlwaysDreamingBig <darkthereal1@gmail.com>
2025-01-12 21:08:24 +0100

    Assets

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -34,3 +34,12 @@ export const extractEmailFromPath = (path: string): string | null => {
     ? decodeURIComponent(potentialEmail)
     : null;
 };
+
+/**
+ * Delays the execution by a specified number of milliseconds.
+ * @param ms - The number of milliseconds to delay.
+ * @returns A Promise that resolves after the specified delay.
+ */
+export const customDelay = (ms: number): Promise<void> => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
