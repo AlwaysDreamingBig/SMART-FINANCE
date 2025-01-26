51a9429e7358d401a842968c0ad20f05babc44ba
AlwaysDreamingBig <darkthereal1@gmail.com>
2025-01-26 20:23:33 +0100

    (root) layout

--- frontend/app/(root)/layout.tsx
+++ frontend/app/(root)/layout.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Sidebar from "@/components/menus/MainMenu";
 
-const layout = () => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>
-      <Sidebar />
+    <div className="flex h-screen bg-gray-200 p-1">
+      {/* Sidebar */}
+      <section className="sm:w-24">
+        <Sidebar />
+      </section>
+
+      {/* Main content */}
+      <section className="flex-1 overflow-auto rounded-lg bg-white p-2">
+        {children}
+      </section>
     </div>
   );
 };
 
-export default layout;
+export default Layout;
