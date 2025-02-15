624f9ce263ccaa9b774c66136085bba9dc1a1af8
AlwaysDreamingBig <darkthereal1@gmail.com>
2025-02-15 14:23:48 +0100

    PFM Layout

---
+++ frontend/app/(root)/PFM/layout.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import PfmNavigation from "@/components/menus/PFM_Navigation";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex max-h-screen flex-col space-y-10">
+      <div className="sticky top-0 z-50 bg-white">
+        <PfmNavigation />
+      </div>
+
+      <main className="grow overflow-auto">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
