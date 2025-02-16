90be938ee2e3084708e096ab066526ddb4ee945f
AlwaysDreamingBig <darkthereal1@gmail.com>
2025-02-16 15:04:40 +0100

    patch background

--- frontend/components/General/Modal.tsx
+++ frontend/components/General/Modal.tsx
@@ -37,7 +37,7 @@ const Modal = ({
   };
 
   return (
-    <div className="bg-opacity/50 fixed inset-0 z-50 flex items-center justify-center bg-black p-4">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 p-4">
       <div
         className={`size-full ${sizeClasses[size]} overflow-hidden rounded-lg bg-white shadow-lg`}
       >
